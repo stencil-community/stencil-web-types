a3404b9b38d37e65e48a9274c53ce9765b421b76
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2024-03-28 09:30:46 -0400

    proto(): stub out a custom stencil output target
    add stencil as a dev dependnecy so that we can use it's types and
    build out a basic output target function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-function talk(msg: string): void {
-  console.log(msg);
-}
+import type {BuildCtx, CompilerCtx, Diagnostic, JsonDocs, OutputTargetCustom} from '@stencil/core/internal';
+import type {Config } from "@stencil/core";
 
-talk("Hello World");
+export const webTypesOutputTarget = (): OutputTargetCustom => ({
+  type: 'custom',
+  name: 'web-types',
+  validate(config: Config, diagnostics: Diagnostic[]) {
+    return true;
+  },
+  async generator(config: Config, compilerCtx: CompilerCtx, buildCtx: BuildCtx, docs: JsonDocs) {
+    const timespan = buildCtx.createTimeSpan('generate web-types started', true);
+
+    console.log('This is where the impl would live!');
+
+    timespan.finish('generate web-types finished');
+  },
+});
